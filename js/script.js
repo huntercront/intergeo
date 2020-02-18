1aac64cd7397d69f536e3e7bb8f22451d901bcaa
huntercront <38252870+huntercront@users.noreply.github.com>
2020-02-18 17:52:03 +0300

    add mobyle adapt

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 	ymaps.ready(function () {
 var myMap = new ymaps.Map('map', {
-center: [55.76054506898901,37.5994625],
+	center: [55.76054506898901,37.5994625],
 zoom: 15,
 controls: ['zoomControl'],
 behaviors: ['drag']
@@ -16,19 +16,60 @@ MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
 myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
 hintContent: '<div class="hint-map">Тверской бул., Дом 13, корпус 1, Москва, Россия, 123104</div>',
 }, {
-// Опции.
-// Необходимо указать данный тип макета.
+
+
 iconLayout: 'default#image',
-// Своё изображение иконки метки.
+
 iconImageHref: './img/map-icon.svg',
-// Размеры метки.
-// iconImageSize: [100, 42],
+
 iconImageSize: [60, 60],
 iconImageOffset: [-35, -35]
-});
-myMap.geoObjects
-.add(myPlacemark)
-// .add(myPlacemarkWithContent);
+}),
+myPlacemark3 = new ymaps.Placemark([54.584222181163646,36.38552449999999], {
+	hintContent: '<div class="hint-map">Moscow</div>',
+
+}, {
+		iconLayout: 'default#image',
+		iconImageHref: './img/map-icon.svg',
+		iconImageSize: [60, 60],
+		iconImageOffset: [-35, -35]
+	});
+
+	myMap.geoObjects
+	.add(myPlacemark)
+
+$(".nav-contacts-link").on("click",function(e){
+	$('.nav-contacts-link').removeClass('nav-items-active')
+	$(this).addClass('nav-items-active');
+
+	var activeWidth = $(this).innerWidth();
+	var itemPos = $(this).position();
+	$(".selector-nav-contacts").css({
+		"left":itemPos.left + "px", 
+		"width": activeWidth + "px"
+	});
+
+	$('.tab-contacts').removeClass('active-tab');
+	$('[data-tab='+$(this).attr('data-select')+']').addClass('active-tab');
+	if($(this).attr('data-select')==1){
+		myMap.geoObjects
+		.remove(myPlacemark3)
+		myMap.setCenter([40, 50], 3, {
+			checkZoomRange: true
+	});
+		myMap.geoObjects
+		.add(myPlacemark)
+	}
+	else{
+		myMap.geoObjects
+		.remove(myPlacemark)
+		myMap.setCenter([55.76054506898901,37.5994625], 2,{});
+		myMap.geoObjects
+		.add(myPlacemark3)
+	}
+	
+})
+
 });
 });
 
@@ -70,3 +111,8 @@ $('.lang-active').on('click',function(event){
 // 	},
 // 	preventMouse: false
 // });
+
+$(".selector-nav-contacts").css({
+	"left":$('.nav-items-active').position().left + "px", 
+	"width": $('.nav-items-active').innerWidth() + "px"
+});
